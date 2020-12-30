b153b583ebf0b6bba3b824a87411b0358ea25878
Grzegorz Bielanski <gbielanski@poczta.onet.pl>
2020-12-30 08:50:52 +0100

    Initial Commit

---
+++ FavRestaurants/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FavRestaurants
+//
+//  Created by Grzegorz Bielanski on 30/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
