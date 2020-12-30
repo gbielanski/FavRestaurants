a88841990c63875c093249a3ce7df5ae15cacc73
Grzegorz Bielanski <gbielanski@poczta.onet.pl>
2020-12-30 20:42:13 +0100

    Add initial response parsing

--- FavRestaurants/Networking/ZomatoResponses/Restaurant.swift
+++ FavRestaurants/Networking/ZomatoResponses/Restaurant.swift
@@ -8,5 +8,16 @@
 import Foundation
 
 class Restaurant: Codable{
-  
+  let restaurant: RestaurantData
+}
+
+class RestaurantData: Codable{
+  let name: String
+  let location: Location
+}
+
+class Location: Codable {
+  let address: String
+  let latitude: String
+  let longitude: String
 }
--- FavRestaurants/Networking/ZomatoResponses/SearchResponse.swift
+++ FavRestaurants/Networking/ZomatoResponses/SearchResponse.swift
@@ -8,5 +8,14 @@
 import Foundation
 
 class SearchResponse: Codable{
+//  let resultsFound, resultsStart, resultsShown: Int
   let restaurants: [Restaurant]
+
+  enum CodingKeys: String, CodingKey{
+//    case resultsFound = "results_found"
+//    case resultsStart = "results_start"
+//    case resultsShown = "results_shown"
+    case restaurants
+
+  }
 }
